19ceae008e7776bef08450d24b9f1af1550ce9d6
nkeshu <167253834+nkeshu@users.noreply.github.com>
2024-04-28 14:59:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,67 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
 #include "ShoppingCart.h"
-
 void PrintMenu() {
-   /* Type your code here */
-   
+cout << "MENU" << endl;
+cout << "a - Add item to cart" << endl;
+cout << "d - Remove item from cart" << endl;
+cout << "c - Change item quantity" << endl;
+cout << "i - Output items' descriptions" << endl;
+cout << "o - Output shopping cart" << endl;
+cout << "q - Quit" << endl;
 }
-
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
-   
+string itemName;
+int itemPrice, itemQuantity;
+ItemToPurchase newItem;
+   switch (option) {
+   case 'a':
+   cout << "ADD ITEM TO CART" << endl;
+   cout << "Enter the item name: ";
+   cin.ignore();
+   getline(cin, itemName);
+   newItem.SetName(itemName);
+   cout << "Enter the item description: ";
+   getline(cin, itemName);
+   newItem.SetDescription(itemName);
+   cout << "Enter the item price: ";
+   cin >> itemPrice;
+   newItem.SetPrice(itemPrice);
+   cout << "Enter the item quantity: ";
+   cin >> itemQuantity;
+   newItem.SetQuantity(itemQuantity);
+   theCart.AddItem(newItem);
+   break;
+   case 'd':
+   cout << "REMOVE ITEM FROM CART" << endl;
+   cout << "Enter name of item to remove: ";
+   cin.ignore();
+   getline(cin, itemName);
+   theCart.RemoveItem(itemName);
+   break;
+   case 'c':
+   cout << "CHANGE ITEM QUANTITY" << endl;
+   cout << "Enter the item name: ";
+   cin.ignore();
+   getline(cin, itemName);
+   newItem.SetName(itemName);
+   cout << "Enter the new quantity: ";
+   cin >> newQuantity;
+   newItem.SetQuantity(newQuantity);
+   theCart.ModifyItem(newItem);
+   break;
+   case 'i':
+   theCart.PrintDescriptions();
+   break;
+   case 'o':
+   theCart.PrintTotal();
+   break;
+   case 'q':
+   break;
+   default:
+   //WILL HAVE TO FIX SOON
+    }
 }
 
 int main() {
